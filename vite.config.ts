8f6132f32888e4848c9ef11a8ee2d0270a7c60a1
Larry Lewise <larry@fruitvc.com>
2025-01-17 10:01:23 -0600

    updated the packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,3 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
-export default defineConfig({
- base: '/',
- plugins: [react()],
- server: {
-   host: "::",
-   port: 8080,
- },
- resolve: {
-   alias: {
-     "@": path.resolve(__dirname, "./src"),
-   },
-   extensions: ['.js', '.jsx', '.ts', '.tsx']
- },
- build: {
-   outDir: 'dist',
-   commonjsOptions: {
-     include: [/node_modules/],
-     transformMixedEsModules: true
-   },
-   rollupOptions: {
-     output: {
-       manualChunks: undefined
-     }
-   }
- }
-});
